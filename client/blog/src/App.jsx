2e87dc16ac32da8687a8b6f1c515507fe8b90207
thisaramax20 <thisarathejana20@gmail.com>
2025-02-23 19:35:45 +0530

    add imagekit

--- client/blog/src/App.jsx
+++ client/blog/src/App.jsx
@@ -1,5 +1,12 @@
+import Navbar from "./components/Navbar";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+      {/* Navbar */}
+      <Navbar />
+    </div>
+  );
 };
 
 export default App;
---
+++ client/blog/src/components/Navbar.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import Image from "./Image";
+
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  return (
+    <div className="w-full h-16 md:h-28 flex items-center justify-between">
+      {/* LOGO */}
+      <div className="flex items-center gap-4 text-2xl font-bold">
+        <Image src="logo.svg" alt="logo" w={32} h={32} />
+        <span>Blog</span>
+      </div>
+      {/* MOBILE MENU */}
+      <div className="md:hidden">
+        <div
+          className="cursor-pointer text-4xl"
+          onClick={() => setOpen((prev) => !prev)}
+        >
+          {open ? "X" : "m"}
+        </div>
+        {/* MOBILE LINK LIST */}
+        <div
+          className={`w-full h-screen flex flex-col transition-all ease-in-out items-center justify-center absolute top-16 bg-red-200 ${
+            open ? "-right-0" : "-right-[100%]"
+          }`}
+        >
+          Menu
+        </div>
+      </div>
+
+      {/* DESKTOP MENU */}
+      <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
+        <a href="/">Home</a>
+        <a href="/">Trending</a>
+        <a href="/">Most Popular</a>
+        <a href="/">About</a>
+        <a href="/">
+          <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
+            Login
+          </button>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
